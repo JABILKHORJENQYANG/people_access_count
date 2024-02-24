fecc286b6e8fb47a91edb132635707920d4aff52
khorjenqyang <jenqyang_khor@jabil.com>
2024-02-24 20:42:42 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+
+const app = express();
+const port = 3000
+
+//server variable
+let count = 0
+
+app.get("/",(req,res)=>{
+    count = count + 1
+    res.send(`${count} People Access this server`);
+})
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port} ...`)
+})
